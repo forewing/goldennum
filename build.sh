26a5152f2f9fba168133f71453f568f2bbe225c8
forewing <jujianai@hotmail.com>
2020-07-27 15:56:46 +0800

    Add packr2 to pack resource files into binary

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@
 rm -rf output
 mkdir -p output
 
+# Install packr2, go to / to prevent updating go.mod
+(cd / && go get github.com/gobuffalo/packr/v2/packr2)
+
+# Build resources files
+packr2
+
+# Build server
 go build -ldflags "-s -w"
 mv goldennum output/
-
-cp -r templates output/
-cp -r statics output/

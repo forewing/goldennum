eab99c8f8502399a56f5079a579e5642b2a84ce2
forewing <jujianai@hotmail.com>
2020-07-27 16:57:19 +0800

    Fix gomod error

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ mkdir -p output
 # Install packr2, go to / to prevent updating go.mod
 (cd / && go get github.com/gobuffalo/packr/v2/packr2)
 
+export GO111MODULE=on
+
 # Build resources files
 packr2
 

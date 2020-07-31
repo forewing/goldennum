8c99cbd5d0db9b2e205780b40841541bf5144a5a
forewing <jujianai@hotmail.com>
2020-08-01 00:13:54 +0800

    Add roomHistory chart

--- statics/components/dashboard.js
+++ statics/components/dashboard.js
@@ -4,20 +4,91 @@ Vue.component('dashboard', {
     data: function () {
         return {
             data: "{}",
+            roomHistoryCtx: null,
+            roomHistoryChart: null,
+            roomHistoryData: {
+                type: "line",
+                data: {
+                    labels: [],
+                    datasets: [{
+                        label: 'Goldennum',
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [],
+                        fill: false,
+                    }]
+                },
+                options: {
+                    legend: false,
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Goldennum History'
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Rounds'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Goldennum'
+                            },
+                            ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 20,
+                            }
+                        }]
+                    }
+                },
+            },
+            userRankCtx: null,
+            userRankChart: null,
+            userRankData: {},
         }
     },
     methods: {
-        refreshRoom() {
+        refreshRoomAction() {
             localStorage.setItem(KEY_ROOM_ID, 1);
             const roomId = localStorage.getItem(KEY_ROOM_ID);
             if (!roomId) {
                 return;
             }
-            fetch(URL_ROOM_INFO + roomId)
-                .then(response => response.json())
-                .then(data => {
-                    this.data = data;
-                });
+            getRoomInfo(roomId).then(data => {
+                this.refreshRoom(data);
+            }).catch(error => {
+                console.error(error);
+            });
+        },
+        refreshRoom(data) {
+            this.data = data;
+            if (!data.RoomHistorys) {
+                return;
+            }
+            this.roomHistoryChart.data.labels = [];
+            this.roomHistoryChart.data.datasets[0].data = [];
+            for (const history of data.RoomHistorys) {
+                if (history.Round == null || history.GoldenNum == null) {
+                    continue;
+                }
+                this.roomHistoryChart.data.labels.push(history.Round);
+                this.roomHistoryChart.data.datasets[0].data.push(history.GoldenNum);
+            }
+            this.roomHistoryChart.update();
         }
-    }
+    },
+    mounted() {
+        this.roomHistoryCtx = this.$refs.roomHistory.getContext('2d')
+        this.roomHistoryChart = new Chart(this.roomHistoryCtx, this.roomHistoryData);
+        this.userRankCtx = this.$refs.userRank.getContext('2d');
+    },
 })

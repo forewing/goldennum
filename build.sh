366998fde0a20a55cf43e932f61374f6afea2428
forewing <jujianai@hotmail.com>
2021-03-21 11:10:10 +0800

    Check exit code directly, not with $?

--- build.sh
+++ build.sh
@@ -6,13 +6,11 @@ mkdir -p output
 VERSION_PACKAGE="github.com/forewing/goldennum/version"
 LDFLAGS="-s -w"
 
-GIT_TAG=$(git describe --tags)
-if [ $? -eq 0 ]; then
+if GIT_TAG=$(git describe --tags); then
     LDFLAGS="$LDFLAGS -X '$VERSION_PACKAGE.Version=$GIT_TAG'"
 fi
 
-GIT_HASH=$(git rev-parse HEAD)
-if [ $? -eq 0 ]; then
+if GIT_HASH=$(git rev-parse HEAD); then
     LDFLAGS="$LDFLAGS -X '$VERSION_PACKAGE.Hash=$GIT_HASH'"
 fi
 
